d58480840666c53e486f2c19a1eb68a3cefec12b
egpelliarmus <s32210073@student.ubm.ac.id>
2023-09-15 10:23:07 +0700

    first commit

---
+++ bin/tutorial_dart_dasar.dart
@@ -0,0 +1,5 @@
+import 'package:tutorial_dart_dasar/tutorial_dart_dasar.dart' as tutorial_dart_dasar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tutorial_dart_dasar.calculate()}!');
+}
---
+++ test/tutorial_dart_dasar_test.dart
@@ -0,0 +1,8 @@
+import 'package:tutorial_dart_dasar/tutorial_dart_dasar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
